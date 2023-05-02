f687ae3587c7eac3937870e8a21f823cfbc23ce1
Eleonora907 <elenatroyan2010@ukr.net>
2023-05-02 17:27:58 +0200

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+  let intervalId = null;
 
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  function startInterval() {
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+  }
+
+  function stopInterval() {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+  }
+
+  startBtn.addEventListener('click', startInterval);
+  stopBtn.addEventListener('click', stopInterval);
---
+++ src/js/02-timer.js
@@ -0,0 +1,60 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from "notiflix";
+
+const datePicker = document.querySelector("#datetime-picker");
+const startButton = document.querySelector('[data-start]');
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minutesValue = document.querySelector('[data-minutes]');
+const secondsValue = document.querySelector('[data-seconds]');
+
+// Створюємо flatpickr і додаємо обробник події onClose
+flatpickr(datePicker, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = new Date(selectedDates[0]);
+    selectedDate.setMilliseconds(0);
+    const currentDate = new Date();
+    if (selectedDate < currentDate) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startButton.setAttribute('disabled', 'disabled');
+      startButton.removeAttribute('data-start-date');
+    } else {
+      startButton.removeAttribute('disabled');
+      startButton.setAttribute('data-start-date', selectedDate.toISOString());
+    }
+  },
+});
+
+// Прослуховуємо клік на кнопці "Start"
+startButton.addEventListener('click', () => {
+  if (!startButton.hasAttribute('disabled')) {
+    countdown(startButton.getAttribute('data-start-date'));
+    startButton.setAttribute('disabled', 'disabled');
+  }
+});
+
+// Функція відліку
+function countdown(targetDate) {
+    const intervalId = setInterval(() => {
+      const currentDate = new Date();
+      const timeDifference = Date.parse(targetDate) - currentDate;
+      if (timeDifference <= 0) {
+        clearInterval(intervalId);
+        return;
+      }
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24)).toString().padStart(2, '0');
+      const hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24).toString().padStart(2, '0');
+      const minutes = Math.floor((timeDifference / (1000 * 60)) % 60).toString().padStart(2, '0');
+      const seconds = Math.floor((timeDifference / 1000) % 60).toString().padStart(2, '0');
+  
+      daysValue.textContent = days;
+      hoursValue.textContent = hours;
+      minutesValue.textContent = minutes;
+      secondsValue.textContent = seconds;
+    }, 1000);
+  }
